e6eac73f77dc0821cf9a837fea2233f92f88f761
Patrick Stewart <patstew@gmail.com>
2013-07-02 10:07:22 +0100

    Add convenience functions to new Py class. Supports "using (Py.GIL()) {}" blocks to setup the python interpreter and take/release the GIL, and Py.kw("key1", value1, "key2", value2, ...) to add keyword arguments.

--- pythonnet/src/runtime/pythonengine.cs
+++ pythonnet/src/runtime/pythonengine.cs
@@ -352,10 +352,61 @@ namespace Python.Runtime {
             }
             return new PyObject(result);
         }
-
-
-
     }
 
+    public static class Py
+    {
+        public static GILState GIL()
+        {
+            if (!PythonEngine.IsInitialized)
+                PythonEngine.Initialize();
 
+            return new GILState();
+        }
+
+        public class GILState : IDisposable
+        {
+            private IntPtr state;
+            internal GILState()
+            {
+                state = PythonEngine.AcquireLock();
+            }
+            public void Dispose()
+            {
+                PythonEngine.ReleaseLock(state);
+                GC.SuppressFinalize(this);
+            }
+            ~GILState()
+            {
+                Dispose();
+            }
+        }
+
+        public class KeywordArguments : PyDict { }
+
+        public static KeywordArguments kw(params object[] kv)
+        {
+            var dict = new KeywordArguments();
+            if (kv.Length % 2 != 0)
+                throw new ArgumentException("Must have an equal number of keys and values");
+            for (int i = 0; i < kv.Length; i += 2)
+            {
+                IntPtr value;
+                if (kv[i + 1] is PyObject)
+                    value = ((PyObject)kv[i + 1]).Handle;
+                else
+                    value = Converter.ToPython(kv[i + 1], kv[i + 1].GetType());
+                if (Runtime.PyDict_SetItemString(dict.Handle, (string)kv[i], value) != 0)
+                    throw new ArgumentException(string.Format("Cannot add key '{0}' to dictionary.", (string)kv[i]));
+                if (!(kv[i + 1] is PyObject))
+                    Runtime.Decref(value);
+            }
+            return dict;
+        }
+
+        public static PyObject Import(string name)
+        {
+            return PythonEngine.ImportModule(name);
+        }
+    }
 }
