b7e85f07d7dd2c84d54b0bb4be4be9a810a81d6a
Brisingr Aerowing <ztgreve@live.com>
2015-09-20 15:59:21 -0400

    Added Python 3.5 Support

--- src/runtime/converter.cs
+++ src/runtime/converter.cs
@@ -405,7 +405,7 @@ namespace Python.Runtime {
                 return true;
 
             case TypeCode.Int32:
-#if !(PYTHON32 || PYTHON33 || PYTHON34)
+#if !(PYTHON32 || PYTHON33 || PYTHON34 || PYTHON35)
                 // Trickery to support 64-bit platforms.
                 if (IntPtr.Size == 4) {
                     op = Runtime.PyNumber_Int(value);
@@ -456,7 +456,7 @@ namespace Python.Runtime {
                 return true;
 
             case TypeCode.Byte:
-#if (PYTHON32 || PYTHON33 || PYTHON34)
+#if (PYTHON32 || PYTHON33 || PYTHON34 || PYTHON35)
                 if (Runtime.PyObject_TypeCheck(value, Runtime.PyBytesType))
                 {
                     if (Runtime.PyBytes_Size(value) == 1)
@@ -496,7 +496,7 @@ namespace Python.Runtime {
                 return true;
 
             case TypeCode.SByte:
-#if (PYTHON32 || PYTHON33 || PYTHON34)
+#if (PYTHON32 || PYTHON33 || PYTHON34 || PYTHON35)
                 if (Runtime.PyObject_TypeCheck(value, Runtime.PyBytesType)) {
                     if (Runtime.PyBytes_Size(value) == 1) {
                         op = Runtime.PyBytes_AS_STRING(value);
@@ -534,7 +534,7 @@ namespace Python.Runtime {
                 return true;
 
             case TypeCode.Char:
-#if (PYTHON32 || PYTHON33 || PYTHON34)
+#if (PYTHON32 || PYTHON33 || PYTHON34 || PYTHON35)
                 if (Runtime.PyObject_TypeCheck(value, Runtime.PyBytesType)) {
                     if (Runtime.PyBytes_Size(value) == 1) {
                         op = Runtime.PyBytes_AS_STRING(value);
