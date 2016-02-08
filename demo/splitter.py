eb34a704211d26f5c2009f729cf55b7574314f49
stonebig <stonebig34@gmail.com>
2016-02-08 21:29:01 +0100

    missing "import clr" in splitter example (develop)

--- demo/splitter.py
+++ demo/splitter.py
@@ -7,6 +7,7 @@
 # FOR A PARTICULAR PURPOSE.
 # ===========================================================================
 
+import clr
 import System.Windows.Forms as WinForms
 from System.Drawing import Color, Size, Point
 import System
