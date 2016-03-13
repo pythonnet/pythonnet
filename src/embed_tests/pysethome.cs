e6294a77897ca00ab6e3d90e06f62b23a1adea68
leo <leo.kent@gmail.com>
2016-03-13 14:01:04 +0000

    Unit test for verifying Python27 home path is correctly set.

---
+++ src/embed_tests/pysethome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Python.Runtime;
+
+
+namespace Python.EmbeddingTest
+{
+    public class PySetHomeSet
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            PythonEngine.Shutdown();
+        }
+
+        [Test]
+        public void TestSetHome()
+        {
+            string homePath = @"C:\Python27\";
+            PythonEngine.PythonHome = homePath;
+            PythonEngine.Initialize();
+            Assert.AreEqual(PythonEngine.PythonHome, homePath);
+        }
+    }
+}
