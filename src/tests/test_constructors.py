f7b71f592a9c0a9d4bfab83023339e02adc97b90
Victor Uriarte <victor.m.uriarte@intel.com>
2017-01-08 21:20:46 -0700

    Unneeded print in unittests

--- src/tests/test_constructors.py
+++ src/tests/test_constructors.py
@@ -44,7 +44,6 @@ class ConstructorTests(unittest.TestCase):
 
         instance = sub()
         ob = SubclassConstructorTest(instance)
-        print(ob)
         self.assertTrue(isinstance(ob.value, System.Exception))
 
 
