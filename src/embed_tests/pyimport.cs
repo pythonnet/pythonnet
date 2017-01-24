8baf9d336e36a3bba99ddee6604b6f0659a6c7a8
Benedikt Reinartz <filmor@gmail.com>
2017-01-24 15:56:42 +0100

    Simplify the `sys.path` setting in the PyImportTests.

--- src/embed_tests/pyimport.cs
+++ src/embed_tests/pyimport.cs
@@ -26,25 +26,13 @@ namespace Python.EmbeddingTest
              */
             const string s = @"../../../../tests";
 
-            Type RTClass = typeof(Runtime.Runtime);
+            var testPath = Path.Combine(
+                TestContext.CurrentContext.TestDirectory, s
+                );
 
-            /* pyStrPtr = PyString_FromString(s); */
-            MethodInfo PyString_FromString = RTClass.GetMethod("PyString_FromString",
-                BindingFlags.NonPublic | BindingFlags.Static);
-            object[] funcArgs = new object[1];
-            funcArgs[0] = s;
-            IntPtr pyStrPtr = (IntPtr)PyString_FromString.Invoke(null, funcArgs);
-
-            /* SysDotPath = sys.path */
-            MethodInfo PySys_GetObject = RTClass.GetMethod("PySys_GetObject",
-                BindingFlags.NonPublic | BindingFlags.Static);
-            funcArgs[0] = "path";
-            IntPtr SysDotPath = (IntPtr)PySys_GetObject.Invoke(null, funcArgs);
-
-            /* SysDotPath.append(*pyStrPtr) */
-            MethodInfo PyList_Append = RTClass.GetMethod("PyList_Append", BindingFlags.NonPublic | BindingFlags.Static);
-            funcArgs = new object[] { SysDotPath, pyStrPtr };
-            int r = (int)PyList_Append.Invoke(null, funcArgs);
+            IntPtr str = Runtime.Runtime.PyString_FromString(testPath);
+            IntPtr path = Runtime.Runtime.PySys_GetObject("path");
+            Runtime.Runtime.PyList_Append(path, str);
         }
 
         [TearDown]
