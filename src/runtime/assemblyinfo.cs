58d2459eab237bbf99569d5ceaefa47e01190843
Benedikt Reinartz <filmor@gmail.com>
2017-01-24 15:54:28 +0100

    Make internals (in particular Runtime.*) visible to the tests.

--- src/runtime/assemblyinfo.cs
+++ src/runtime/assemblyinfo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyProduct("Python for .NET")]
 [assembly: AssemblyVersion("4.0.0.1")]
@@ -12,23 +13,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("2.0.0.2")]
 [assembly: NeutralResourcesLanguage("en")]
 
-#if PYTHON27
-[assembly: AssemblyTitle("Python.Runtime for Python 2.7")]
-[assembly: AssemblyDescription("Python Runtime for Python 2.7")]
-#endif
-#if PYTHON33
-[assembly: AssemblyTitle("Python.Runtime for Python 3.3")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.3")]
-#endif
-#if PYTHON34
-[assembly: AssemblyTitle("Python.Runtime for Python 3.4")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.4")]
-#endif
-#if PYTHON35
-[assembly: AssemblyTitle("Python.Runtime for Python 3.5")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.5")]
-#endif
-#if PYTHON36
-[assembly: AssemblyTitle("Python.Runtime for Python 3.6")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.6")]
-#endif
+[assembly: InternalsVisibleTo("Python.EmbeddingTest")]
