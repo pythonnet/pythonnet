ba7f80fff33d42bba6b3f7089bd19a2d6bea4974
Benedikt Reinartz <filmor@gmail.com>
2017-01-24 15:54:28 +0100

    Make internals (in particular Runtime.*) visible to the tests.

--- src/runtime/assemblyinfo.cs
+++ src/runtime/assemblyinfo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyProduct("Python for .NET")]
 [assembly: AssemblyVersion("4.0.0.1")]
@@ -11,23 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyFileVersion("2.0.0.2")]
 [assembly: NeutralResourcesLanguage("en")]
-
-#if PYTHON27
-[assembly: AssemblyTitle("Python.Runtime for Python 2.7")]
-[assembly: AssemblyDescription("Python Runtime for Python 2.7")]
-#elif PYTHON33
-[assembly: AssemblyTitle("Python.Runtime for Python 3.3")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.3")]
-#elif PYTHON34
-[assembly: AssemblyTitle("Python.Runtime for Python 3.4")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.4")]
-#elif PYTHON35
-[assembly: AssemblyTitle("Python.Runtime for Python 3.5")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.5")]
-#elif PYTHON36
-[assembly: AssemblyTitle("Python.Runtime for Python 3.6")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.6")]
-#elif PYTHON37
-[assembly: AssemblyTitle("Python.Runtime for Python 3.7")]
-[assembly: AssemblyDescription("Python Runtime for Python 3.7")]
-#endif
+[assembly: InternalsVisibleTo("Python.EmbeddingTest")]
