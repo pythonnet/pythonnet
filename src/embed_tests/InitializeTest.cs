689c1fcd40b39e727bef9edd2122463a22abd28f
Benedikt Reinartz <filmor@gmail.com>
2017-01-24 16:05:43 +0100

    Add Initialize test and update NUnit to 3.5.0

---
+++ src/embed_tests/InitializeTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Python.EmbeddingTest
+{
+    public class InitializeTest
+    {
+        [Test]
+        public static void Test()
+        {
+            PythonEngine.Initialize();
+            PythonEngine.Shutdown();
+
+            PythonEngine.Initialize();
+            PythonEngine.Shutdown();
+        }
+    }
+}
