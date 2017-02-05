198f6545f5ed4c718fb818679df220e049751f84
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-05 11:37:37 -0700

    Remove mono dependency from PY2 as well.

--- src/runtime/runtime.cs
+++ src/runtime/runtime.cs
@@ -1720,9 +1720,28 @@ namespace Python.Runtime
             EntryPoint = "PyUnicodeUCS4_FromUnicode",
             ExactSpelling = true)]
         internal unsafe static extern IntPtr
-            PyUnicode_FromUnicode(
-            [MarshalAs(UnmanagedType.CustomMarshaler,
-                MarshalTypeRef = typeof(Utf32Marshaler))] string s, int size);
+            PyUnicode_FromUnicode(IntPtr s, int size);
+
+        internal static unsafe IntPtr PyUnicode_FromUnicode(string s, int size)
+        {
+            var bufLength = Math.Max(s.Length, size) * 4;
+
+            IntPtr mem = Marshal.AllocHGlobal(bufLength);
+            try
+            {
+                fixed (char* ps = s)
+                {
+                    Encoding.UTF32.GetBytes(ps, s.Length, (byte*)mem, bufLength);
+                }
+
+                var result = PyUnicode_FromUnicode(mem, bufLength);
+                return result;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(mem);
+            }
+        }
 
         [DllImport(Runtime.dll, CallingConvention = CallingConvention.Cdecl,
             EntryPoint = "PyUnicodeUCS4_GetSize",
