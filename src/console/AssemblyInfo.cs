42a6191adb825aef797f902e34854d81081e5296
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-07 14:16:40 -0700

    Remove obsoleted & Unenforced PermissionSet
    See: http://stackoverflow.com/questions/11625447/securityaction-requestminimum-is-obsolete-in-net-4-0

--- src/console/AssemblyInfo.cs
+++ src/console/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Security.Permissions;
 [assembly: AssemblyDefaultAlias("python.exe")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: PermissionSet(SecurityAction.RequestMinimum, Name = "FullTrust")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyFileVersion("2.0.0.4")]
