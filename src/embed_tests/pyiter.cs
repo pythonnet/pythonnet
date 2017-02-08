368d4d175c55d79c896295a8aaec371520403d8e
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-08 12:35:52 -0700

    Simplify embed_tests
    Since the previous bug involving initialize/finalize have been solved,
    we can confidently upgrade these test to use simpler format.
    
    Also Remove Assert fail messages
    NUnit already says the name of which test failed.

--- src/embed_tests/pyiter.cs
+++ src/embed_tests/pyiter.cs
@@ -1,45 +1,30 @@
-using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using Python.Runtime;
 
 namespace Python.EmbeddingTest
 {
-    [TestFixture]
     public class PyIterTest
     {
-        private IntPtr gs;
-
-        [SetUp]
-        public void SetUp()
-        {
-            PythonEngine.Initialize();
-            gs = PythonEngine.AcquireLock();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            PythonEngine.ReleaseLock(gs);
-            PythonEngine.Shutdown();
-        }
-
         [Test]
         public void TestOnPyList()
         {
-            var list = new PyList();
-            list.Append(new PyString("foo"));
-            list.Append(new PyString("bar"));
-            list.Append(new PyString("baz"));
-            var result = new List<string>();
-            foreach (PyObject item in list)
+            using (Py.GIL())
             {
-                result.Add(item.ToString());
+                var list = new PyList();
+                list.Append(new PyString("foo"));
+                list.Append(new PyString("bar"));
+                list.Append(new PyString("baz"));
+                var result = new List<string>();
+                foreach (PyObject item in list)
+                {
+                    result.Add(item.ToString());
+                }
+                Assert.AreEqual(3, result.Count);
+                Assert.AreEqual("foo", result[0]);
+                Assert.AreEqual("bar", result[1]);
+                Assert.AreEqual("baz", result[2]);
             }
-            Assert.AreEqual(3, result.Count);
-            Assert.AreEqual("foo", result[0]);
-            Assert.AreEqual("bar", result[1]);
-            Assert.AreEqual("baz", result[2]);
         }
     }
 }
