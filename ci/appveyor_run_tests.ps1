a5d325f79d599d1e38b7e62a8e13f15641520738
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-13 14:46:17 -0700

    Refactor AppVeyor/Travis embedded test_runner
    Make it easier to diff and upgrade versions by
    breaking into multiple lines and using path completion
    
    Powershell continuation: http://stackoverflow.com/a/2608186/5208670
    Powershell options splatting: http://stackoverflow.com/a/24313253/5208670

--- ci/appveyor_run_tests.ps1
+++ ci/appveyor_run_tests.ps1
@@ -3,7 +3,7 @@
 # Executable paths for OpenCover
 # Note if OpenCover fails, it won't affect the exit codes.
 $OPENCOVER = Resolve-Path .\packages\OpenCover.*\tools\OpenCover.Console.exe
-$NUNIT = Resolve-Path .\packages\NUnit.Runners*\tools\"$env:NUNIT".exe
+$NUNIT = Resolve-Path .\packages\NUnit.*\tools\"$env:NUNIT".exe
 $PY = Get-Command python
 
 # Can't use ".\build\*\Python.EmbeddingTest.dll". Missing framework files.
@@ -13,15 +13,22 @@ $RUNTIME_DIR = ".\src\runtime\bin\"
 # Run python tests with C# coverage
 # why `2>&1 | %{ "$_" }`? see: http://stackoverflow.com/a/20950421/5208670
 Write-Host ("Starting Python tests") -ForegroundColor "Green"
-.$OPENCOVER -register:user -searchdirs:"$RUNTIME_DIR" -output:py.coverage -target:"$PY" -targetargs:src\tests\runtests.py -returntargetcode 2>&1 | %{ "$_" }
+.$OPENCOVER -register:user -searchdirs:"$RUNTIME_DIR" -output:py.coverage `
+            -target:"$PY" -targetargs:src\tests\runtests.py `
+            -returntargetcode `
+            2>&1 | %{ "$_" }
 $PYTHON_STATUS = $LastExitCode
 if ($PYTHON_STATUS -ne 0) {
     Write-Host "Python tests failed, continuing to embedded tests" -ForegroundColor "Red"
 }
 
 # Run Embedded tests with C# coverage
+# Powershell continuation: http://stackoverflow.com/a/2608186/5208670
+# Powershell options splatting: http://stackoverflow.com/a/24313253/5208670
 Write-Host ("Starting embedded tests") -ForegroundColor "Green"
-.$OPENCOVER -register:user -searchdirs:"$RUNTIME_DIR" -output:cs.coverage -target:"$NUNIT" -targetargs:"$CS_TESTS" -returntargetcode
+.$OPENCOVER -register:user -searchdirs:"$RUNTIME_DIR" -output:cs.coverage `
+            -target:"$NUNIT" -targetargs:"$CS_TESTS" `
+            -returntargetcode
 $NUNIT_STATUS = $LastExitCode
 if ($NUNIT_STATUS -ne 0) {
     Write-Host "Embedded tests failed" -ForegroundColor "Red"
