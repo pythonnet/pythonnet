8f30a39a2b53f664e814a470972724c7ccb75567
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-14 21:21:32 -0700

    Use builtin miniconda

--- ci/appveyor_build_recipe.ps1
+++ ci/appveyor_build_recipe.ps1
@@ -1,6 +1,21 @@
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
-    Invoke-Expression .\ci\install_miniconda.ps1
-    &"$env:CONDA_BLD\Scripts\conda" build conda.recipe --dirty -q
-    $CONDA_PKG=(&"$env:CONDA_BLD\Scripts\conda" build conda.recipe --output -q)
+    # Update PATH, and keep a copy to restore at end of this PowerShell script
+    $old_path = $env:path
+    $env:path = "$env:CONDA_BLD;$env:CONDA_BLD\Scripts;" + $env:path
+
+    Write-Host "Starting Conda Update/Install" -ForegroundColor "Green"
+    conda update conda -q -y
+    conda install conda-build jinja2 anaconda-client -q -y
+
+    Write-Host "Starting Conda Recipe build" -ForegroundColor "Green"
+    conda build conda.recipe -q --dirty
+
+    $CONDA_PKG=(conda build conda.recipe -q --output)
     Copy-Item $CONDA_PKG "$env:APPVEYOR_BUILD_FOLDER\dist\"
+    Write-Host "Completed Conda Recipe build" -ForegroundColor "Green"
+
+    # Restore PATH back to original
+    $env:path = $old_path
+} else {
+    Write-Host "Skipping Conda Recipe build" -ForegroundColor "Green"
 }
