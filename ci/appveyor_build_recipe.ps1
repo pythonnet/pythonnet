433dc33edc2872daefb2fda0e3d4fe1b55d5858c
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-16 03:10:16 -0700

    Clean-up AppVeyor build recipe
    Conda automatically updates when doing install. To disable its autoupdate add
    
        conda config --set auto_update_conda False

--- ci/appveyor_build_recipe.ps1
+++ ci/appveyor_build_recipe.ps1
@@ -1,21 +1,24 @@
+# Build `conda.recipe` only if this is a Pull_Request. Saves time for CI.
+
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
     # Update PATH, and keep a copy to restore at end of this PowerShell script
     $old_path = $env:path
     $env:path = "$env:CONDA_BLD;$env:CONDA_BLD\Scripts;" + $env:path
 
-    Write-Host "Starting Conda Update/Install" -ForegroundColor "Green"
-    conda update conda -q -y
-    conda install conda-build jinja2 anaconda-client -q -y
+    Write-Host "Starting conda install" -ForegroundColor "Green"
+    conda config --set always_yes True
+    conda config --set changeps1 False
+    conda install conda-build jinja2 anaconda-client --quiet
 
-    Write-Host "Starting Conda Recipe build" -ForegroundColor "Green"
-    conda build conda.recipe -q --dirty
+    Write-Host "Starting conda build recipe" -ForegroundColor "Green"
+    conda build conda.recipe --dirty --quiet
 
-    $CONDA_PKG=(conda build conda.recipe -q --output)
-    Copy-Item $CONDA_PKG "$env:APPVEYOR_BUILD_FOLDER\dist\"
-    Write-Host "Completed Conda Recipe build" -ForegroundColor "Green"
+    $CONDA_PKG=(conda build conda.recipe --output)
+    Copy-Item $CONDA_PKG .\dist\
+    Write-Host "Completed conda build recipe" -ForegroundColor "Green"
 
     # Restore PATH back to original
     $env:path = $old_path
 } else {
-    Write-Host "Skipping Conda Recipe build" -ForegroundColor "Green"
+    Write-Host "Skipping conda build recipe" -ForegroundColor "Green"
 }
