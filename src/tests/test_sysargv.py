9e1a0e482224f4de40547a47800f843c39c75e03
Benedikt Reinartz <filmor@gmail.com>
2017-02-24 15:18:18 +0100

    Fix the issue of sys.argv being cleared on import clr.
    Closes #404.

--- src/tests/test_sysargv.py
+++ src/tests/test_sysargv.py
@@ -4,12 +4,9 @@
 
 import sys
 
-import pytest
-
 from ._compat import check_output
 
 
-@pytest.mark.xfail(reason="argv being reset on import clr. See gh#404")
 def test_sys_argv_state(filepath):
     """Test sys.argv state doesn't change after clr import.
     To better control the arguments being passed, test on a fresh python
