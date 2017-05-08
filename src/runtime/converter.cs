f92feea57b421da5935df78b1f0e762229bbbbc5
dhirschf <david.hirschfeld@stanwell.com>
2017-05-08 17:18:19 +1000

    Allow passing None for nullable args

--- src/runtime/converter.cs
+++ src/runtime/converter.cs
@@ -318,6 +318,17 @@ namespace Python.Runtime
                 return true;
             }
 
+            if (obType.IsGenericType && obType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                if( value == Runtime.PyNone )
+                {
+                    result = null;
+                    return true;
+                }
+                // Set type to underlying type
+                obType = obType.GetGenericArguments()[0];
+            }
+
             if (obType.IsArray)
             {
                 return ToArray(value, obType, out result, setError);
