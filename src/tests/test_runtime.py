1a85eef6465b635deb9a5dfee9aeb4bc4be6bc62
Rickard Holmberg <rickard@avkrok.net>
2017-06-16 00:55:18 +0200

    Add PyIter_Check and PyObject_IsIterable tests on threading.Lock, which does not have type feature iter

--- src/tests/test_runtime.py
+++ src/tests/test_runtime.py
@@ -7,6 +7,7 @@ import System
 import pytest
 import clr
 import Python.Runtime
+import threading
 
 def test_pyobject_isiterable_on_list():
     """Tests that Runtime.PyObject_IsIterable is true for lists ."""
@@ -31,3 +32,19 @@ def test_pyiter_check_on_listiterator():
     ip = System.IntPtr.op_Explicit(System.Int64(id(x)))
     m=assy.GetType("Python.Runtime.Runtime").GetMethod("PyIter_Check", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
     assert m.Invoke(None, [ip]) == True
+
+def test_pyiter_check_on_threadlock():
+    """Tests that Runtime.PyIter_Check is false for threading.Lock, which uses a different code path in PyIter_Check."""
+    assy=clr.AddReference("Python.Runtime")
+    x = threading.Lock()
+    ip = System.IntPtr.op_Explicit(System.Int64(id(x)))
+    m=assy.GetType("Python.Runtime.Runtime").GetMethod("PyIter_Check", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+    assert m.Invoke(None, [ip]) == False
+
+def test_pyobject_isiterable_on_threadlock():
+    """Tests that Runtime.PyObject_IsIterable is false for threading.Lock, which uses a different code path in PyObject_IsIterable."""
+    assy=clr.AddReference("Python.Runtime")
+    x = threading.Lock()
+    ip = System.IntPtr.op_Explicit(System.Int64(id(x)))
+    m=assy.GetType("Python.Runtime.Runtime").GetMethod("PyObject_IsIterable", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+    assert m.Invoke(None, [ip]) == False
