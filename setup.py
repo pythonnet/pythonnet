75e5231c15ff132e650d76859e676567c0b8b5c7
dmitriyse <dmitriy.se@gmail.com>
2017-11-08 01:19:38 +0400

    Build system improvements pack - addressing comments to PR and some issues. (#546)
    * vswhere.exe tool now distributed with the pythonnet sources, dotnet-core-2.0-x86 installation step removed.
    
    * .travis.yaml file simplification.
    
    * PYTHONNET_INTEROP_FILE env var introduced to allow working with custom interop*.cs file inside IDE.
    
    * pdb generation improved for all conditions Net 4.0/NetStandard 2.0 x Debug/Release.
    
    * Fix for the #539 classic build fails when VS 2017 installed.
    
    * Build fixes for the fresh Windows 10 SDKS.
    
    * Workaround: Mono downgrade to 5.2.0 to avoid broken mono 5.4.0.201

--- setup.py
+++ setup.py
@@ -41,6 +41,12 @@ kits_suffix = os.path.join("bin", ARCH)
 
 WIN_SDK_KEYS = (
     RegKey(sdk_name="Windows Kit 10.0", key=kits_root,
+           value_name="KitsRoot10", suffix=os.path.join("bin", "10.0.16299.0", ARCH)),
+
+    RegKey(sdk_name="Windows Kit 10.0", key=kits_root,
+           value_name="KitsRoot10", suffix=os.path.join("bin", "10.0.15063.0", ARCH)),
+
+    RegKey(sdk_name="Windows Kit 10.0", key=kits_root,
            value_name="KitsRoot10", suffix=kits_suffix),
 
     RegKey(sdk_name="Windows Kit 8.1", key=kits_root,
@@ -324,6 +330,20 @@ class BuildExtPythonnet(build_ext.build_ext):
 
     def _find_msbuild_tool(self, tool="msbuild.exe", use_windows_sdk=False):
         """Return full path to one of the Microsoft build tools"""
+
+        # trying to search path with help of vswhere when MSBuild 15.0 and higher installed.
+        if tool=="msbuild.exe" and use_windows_sdk==False:
+            try:
+                basePathes = subprocess.check_output(
+                    ["tools\\vswhere\\vswhere.exe", "-latest",
+                     "-version", "[15.0, 16.0)", 
+                     "-requires", "Microsoft.Component.MSBuild",
+                     "-property", "InstallationPath"]).splitlines()
+                if len(basePathes):
+                    return os.path.join(basePathes[0].decode(sys.stdout.encoding or "utf-8"), "MSBuild", "15.0", "Bin", "MSBuild.exe")
+            except:
+                pass # keep trying to search by old method.
+
         # Search in PATH first
         path = spawn.find_executable(tool)
         if path:
@@ -370,7 +390,7 @@ class BuildExtPythonnet(build_ext.build_ext):
         """Return full path to one of the Microsoft build tools"""
         try:
             basePathes = subprocess.check_output(
-                ["vswhere", "-latest",
+                ["tools\\vswhere\\vswhere.exe", "-latest",
                  "-version", "[15.0, 16.0)", 
                  "-requires", "Microsoft.Component.MSBuild",
                  "-property", "InstallationPath"]).splitlines()
