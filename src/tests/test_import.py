994d609f9448246cfc3e2790455944d155a73057
Cronyn, Ivan (London) <ivan.cronyn@man.com>
2018-02-07 17:01:37 +0000

    Fixes issue where private types are returned

--- src/tests/test_import.py
+++ src/tests/test_import.py
@@ -11,3 +11,14 @@ def test_relative_missing_import():
     Relative import in the site-packages folder"""
     with pytest.raises(ImportError):
         from . import _missing_import
+
+
+def test_private_and_public_types_import():
+    """Tests that importing a type where private and public 
+    versions exist imports the public version. In .Net Core 
+    2.0 there are public and private versions of the same 
+    types in different assemblies. For example, there is a 
+    private version of System.Environment in mscorlib.dll, 
+    and a public version in System.Runtime.Extensions.dll."""
+    from System import Environment
+    assert len(Environment.MachineName) > 0
