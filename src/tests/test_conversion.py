36def5d5005781ca8404d1a57faf6531301430d8
Denis Akhiyarov <denis.akhiyarov@gmail.com>
2018-06-03 05:33:45 -0500

    Update test_conversion.py

--- src/tests/test_conversion.py
+++ src/tests/test_conversion.py
@@ -537,9 +537,9 @@ def test_string_conversion():
         ConversionTest().StringField = 1
     
     world = UnicodeString()
-    assert "안녕" = str(world.value)
-    assert "안녕" = str(world.GetString())
-    assert "안녕" = str(world)
+    assert "안녕" == str(world.value)
+    assert "안녕" == str(world.GetString())
+    assert "안녕" == str(world)
 
 
 def test_interface_conversion():
