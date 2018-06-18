28a0d1ef443585bb0ed0c9ad0b6e3983d3391ce8
John Wilkes <>
2018-06-18 07:25:55 -0500

    Addressing code review comments
    This reverts commit 56e52684d99d803576ea6d58d2245019555ea671.

--- src/embed_tests/TestPyObject.cs
+++ src/embed_tests/TestPyObject.cs
@@ -25,46 +25,6 @@ namespace Python.EmbeddingTest
         {
             List<string> expectedMemberNames = new List<string>
             {
-                "__class__",
-                "__delattr__",
-                "__dict__",
-#if PYTHON3
-                "__dir__",
-#endif
-                "__doc__",
-#if PYTHON3
-                "__eq__",
-#endif
-                "__format__",
-#if PYTHON3
-                "__ge__",
-#endif
-                "__getattribute__",
-#if PYTHON3
-                "__gt__",
-#endif
-                "__hash__",
-                "__init__",
-#if PYTHON36
-                "__init_subclass__",
-#endif
-#if PYTHON3
-                "__le__",
-                "__lt__",
-#endif
-                "__module__",
-#if PYTHON3
-                "__ne__",
-#endif
-                "__new__",
-                "__reduce__",
-                "__reduce_ex__",
-                "__repr__",
-                "__setattr__",
-                "__sizeof__",
-                "__str__",
-                "__subclasshook__",
-                "__weakref__",
                 "add",
                 "getNumber",
                 "member1",
@@ -90,14 +50,11 @@ a = MemberNamesTest()
 
             PyObject a = locals.GetItem("a");
 
-            IEnumerable<string> membernames = a.GetDynamicMemberNames();
+            IEnumerable<string> memberNames = a.GetDynamicMemberNames();
 
-            Assert.AreEqual(expectedMemberNames.Count, membernames.Count(), "Unexpected number of members.");
-
-            IEnumerable<Tuple<string, string>> results = expectedMemberNames.Zip(membernames, (x, y) => new Tuple<string, string>(x, y));
-            foreach (Tuple<string, string> result in results)
+            foreach (string expectedName in expectedMemberNames)
             {
-                Assert.AreEqual(result.Item1, result.Item2, "Unexpected member name.");
+                Assert.IsTrue(memberNames.Contains(expectedName), "Could not find member '{0}'.", expectedName);
             }
         }
     }
