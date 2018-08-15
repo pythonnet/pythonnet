5ef579a4b03f3e8fa2acd8c8d15d2111a508a171
Denis Akhiyarov <denis.akhiyarov@gmail.com>
2018-08-15 12:54:15 -0500

    passing pthread support to clang
    based on: https://stackoverflow.com/questions/2391194/what-is-gs-pthread-equiv-in-clang

--- tools/geninterop/geninterop.py
+++ tools/geninterop/geninterop.py
@@ -185,7 +185,7 @@ def preprocess_python_headers():
             defines.extend(("-D", "PYTHON_WITH_WIDE_UNICODE"))
 
     python_h = os.path.join(include_py, "Python.h")
-    cmd = ["clang", "-I"] + include_dirs + defines + ["-E", python_h]
+    cmd = ["clang", "-pthread", "-I"] + include_dirs + defines + ["-E", python_h]
 
     # normalize as the parser doesn't like windows line endings.
     lines = []
