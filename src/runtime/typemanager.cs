9ffb705debc0472927110b2b9029b58b2435f5b8
Benoit Hudson <benoit@imgspc.com>
2018-09-27 21:56:54 -0400

    Make prettier

--- src/runtime/typemanager.cs
+++ src/runtime/typemanager.cs
@@ -447,7 +447,7 @@ namespace Python.Runtime
         }
 
 
-        #region Native Page
+        #region Native Code Page
         /// <summary>
         /// Initialized by InitializeNativeCodePage.
         ///
