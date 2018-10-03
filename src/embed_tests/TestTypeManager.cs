9b74cce92c1782a107b0a84e3c7e4b6254474e0f
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2018-10-03 13:27:53 -0400

    Fix TypeManager test for running on .NET Core under linux/OSX
    We can't check memory protections in an NUnit tests under Microsoft .NET
    runtimes. I was assuming that meant windows only, but the travis CI pointed out
    that it can mean linux as well.

--- src/embed_tests/TestTypeManager.cs
+++ src/embed_tests/TestTypeManager.cs
@@ -31,15 +31,23 @@ namespace Python.EmbeddingTest
             Assert.That(() => { Marshal.WriteInt64(page, 17); }, Throws.Nothing);
             Assert.That(Marshal.ReadInt64(page), Is.EqualTo(17));
 
-            // Mark it read-execute, now we can't write anymore.
-            //
-            // We can't actually test access protection under Windows, because
-            // AccessViolationException is assumed to mean we're in a corrupted
-            // state:
-            //   https://stackoverflow.com/questions/3469368/how-to-handle-accessviolationexception
+            // Mark it read-execute. We can still read, haven't changed any values.
             mapper.SetReadExec(page, len);
             Assert.That(Marshal.ReadInt64(page), Is.EqualTo(17));
-            if (Runtime.Runtime.OperatingSystem != Runtime.Runtime.OperatingSystemType.Windows)
+
+            // Test that we can't write.
+            //
+            // We can't actually test access protection under Microsoft
+            // versions of .NET, because AccessViolationException is assumed to
+            // mean we're in a corrupted state:
+            //   https://stackoverflow.com/questions/3469368/how-to-handle-accessviolationexception
+            //
+            // We can test under Mono but it throws NRE instead of AccessViolationException.
+            //
+            // We can't use compiler flags because we compile with MONO_LINUX
+            // while running on the Microsoft .NET Core during continuous
+            // integration tests.
+            if (System.Type.GetType ("Mono.Runtime") != null)
             {
                 // Mono throws NRE instead of AccessViolationException for some reason.
                 Assert.That(() => { Marshal.WriteInt64(page, 73); }, Throws.TypeOf<System.NullReferenceException>());
