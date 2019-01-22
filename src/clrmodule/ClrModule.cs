daddc6809573123f930380bb29e8d40922d05329
AlexCatarino <alexandre.catarino@gmail.com>
2019-01-22 21:56:03 +0000

    Version bump to 1.0.5.15
    Version bump to match nuget package

--- src/clrmodule/ClrModule.cs
+++ src/clrmodule/ClrModule.cs
@@ -53,7 +53,7 @@ public class clrModule
         {
 #if USE_PYTHON_RUNTIME_VERSION
             // Has no effect until SNK works. Keep updated anyways.
-            Version = new Version("1.0.5.14"),
+            Version = new Version("1.0.5.15"),
 #endif
             CultureInfo = CultureInfo.InvariantCulture
         };
--- src/runtime/resources/clr.py
+++ src/runtime/resources/clr.py
@@ -2,7 +2,7 @@
 Code in this module gets loaded into the main clr module.
 """
 
-__version__ = "1.0.5.14"
+__version__ = "1.0.5.15"
 
 
 class clrproperty(object):
