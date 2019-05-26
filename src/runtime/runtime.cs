1591a71699b8acafe3f13e4a2465e2f00dec838d
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-05-26 16:20:28 +0200

    added GetPyNone() to Runtime

--- src/runtime/runtime.cs
+++ src/runtime/runtime.cs
@@ -536,6 +536,19 @@ namespace Python.Runtime
         public static IntPtr PyNone;
         public static IntPtr Error;
 
+        public static PyObject _none;
+        public static PyObject GetPyNone()
+        {
+            if (_none == null)
+            {
+                var result = Runtime.PyNone;
+                Runtime.XIncref(result);
+                _none= new PyObject(result);
+            }
+
+            return _none;
+        }
+
         /// <summary>
         /// Check if any Python Exceptions occurred.
         /// If any exist throw new PythonException.
