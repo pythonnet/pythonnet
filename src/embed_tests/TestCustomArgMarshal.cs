56ca4c995a0018ca48123bebfe4485b144a6f9de
Victor Milovanov <lost@losttech.software>
2019-06-13 14:30:01 -0700

    a test to ensure it is possible to override argument conversion for inherited methods using an attribute on a derived class

--- src/embed_tests/TestCustomArgMarshal.cs
+++ src/embed_tests/TestCustomArgMarshal.cs
@@ -28,13 +28,27 @@ namespace Python.EmbeddingTest
             }
             Assert.AreEqual(expected: 42, actual: obj.LastArgument);
         }
+
+        [Test]
+        public void MarshallerOverride() {
+            var obj = new DerivedMarshaling();
+            using (Py.GIL()) {
+                dynamic callWithInt = PythonEngine.Eval("lambda o: o.CallWithInt({ 'value': 42 })");
+                callWithInt(obj.ToPython());
+            }
+            Assert.AreEqual(expected: 42, actual: obj.LastArgument);
+        }
     }
 
-[PyArgConverter(typeof(CustomArgConverter))]
-class CustomArgMarshaling {
-    public object LastArgument { get; private set; }
-    public void CallWithInt(int value) => this.LastArgument = value;
-}
+    [PyArgConverter(typeof(CustomArgConverter))]
+    class CustomArgMarshaling {
+        public object LastArgument { get; protected set; }
+        public virtual void CallWithInt(int value) => this.LastArgument = value;
+    }
+
+    // this should override original custom marshaling behavior
+    [PyArgConverter(typeof(CustomArgConverter2))]
+    class DerivedMarshaling: CustomArgMarshaling { }
 
     class CustomArgConverter : DefaultPyArgumentConverter {
         public override bool TryConvertArgument(IntPtr pyarg, Type parameterType, bool needsResolution,
@@ -48,6 +62,21 @@ class CustomArgMarshaling {
 
             int number;
             if (!int.TryParse((string)arg, out number)) return false;
+            arg = number;
+            return true;
+        }
+    }
+
+    class CustomArgConverter2 : DefaultPyArgumentConverter {
+        public override bool TryConvertArgument(IntPtr pyarg, Type parameterType, bool needsResolution,
+            out object arg, out bool isOut) {
+            if (parameterType != typeof(int))
+                return base.TryConvertArgument(pyarg, parameterType, needsResolution, out arg, out isOut);
+            bool isPyObject = base.TryConvertArgument(pyarg, typeof(PyObject), needsResolution,
+                out arg, out isOut);
+            if (!isPyObject) return false;
+            var dict = new PyDict((PyObject)arg);
+            int number = (dynamic)dict["value"];
             arg = number;
             return true;
         }
