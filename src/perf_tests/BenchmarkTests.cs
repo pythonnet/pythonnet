a6c55d4b66bf72442dc3b4441868b58804012d65
Victor Milovanov <lost@losttech.software>
2019-12-19 09:53:08 -0800

    relax performance targets to ~10%, improve perf failure message

--- src/perf_tests/BenchmarkTests.cs
+++ src/perf_tests/BenchmarkTests.cs
@@ -30,14 +30,14 @@ namespace Python.PerformanceTests
         public void ReadInt64Property()
         {
             double optimisticPerfRatio = GetOptimisticPerfRatio(this.summary.Reports);
-            Assert.LessOrEqual(optimisticPerfRatio, 0.68);
+            AssertPerformanceIsBetterOrSame(optimisticPerfRatio, target: 0.66);
         }
 
         [Test]
         public void WriteInt64Property()
         {
             double optimisticPerfRatio = GetOptimisticPerfRatio(this.summary.Reports);
-            Assert.LessOrEqual(optimisticPerfRatio, 0.67);
+            AssertPerformanceIsBetterOrSame(optimisticPerfRatio, target: 0.64);
         }
 
         static double GetOptimisticPerfRatio(
@@ -61,5 +61,14 @@ namespace Python.PerformanceTests
         }
 
         public static string DeploymentRoot => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static void AssertPerformanceIsBetterOrSame(
+            double actual, double target,
+            double wiggleRoom = 1.1, [CallerMemberName] string testName = null) {
+            double threshold = target * wiggleRoom;
+            Assert.LessOrEqual(actual, threshold,
+                $"{testName}: {actual:F3} > {threshold:F3} (target: {target:F3})"
+                + ": perf result is higher than the failure threshold.");
+        }
     }
 }
