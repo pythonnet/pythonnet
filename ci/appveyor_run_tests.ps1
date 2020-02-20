9e3ed3ae69c54dc26e9e7a15c5c9026ccfa7dfce
Victor Milovanov <lost@losttech.software>
2020-02-20 12:33:03 -0800

    report AppVeyor build timings

--- ci/appveyor_run_tests.ps1
+++ ci/appveyor_run_tests.ps1
@@ -1,5 +1,8 @@
 # Script to simplify AppVeyor configuration and resolve path to tools
 
+$stopwatch = [Diagnostics.Stopwatch]::StartNew()
+[array]$timings = @()
+
 # Test Runner framework being used for embedded tests
 $CS_RUNNER = "nunit3-console"
 
@@ -25,6 +28,17 @@ $PY = Get-Command python
 $CS_TESTS = ".\src\embed_tests\bin\Python.EmbeddingTest.dll"
 $RUNTIME_DIR = ".\src\runtime\bin\"
 
+function ReportTime {
+    param([string] $action)
+
+    $timeSpent = $stopwatch.Elapsed
+    $timings += [pscustomobject]@{action=$action; timeSpent=$timeSpent}
+    Write-Host $action " in " $timeSpent -ForegroundColor "Green"
+    $stopwatch.Restart()
+}
+
+ReportTime "Preparation done"
+
 # Run python tests with C# coverage
 Write-Host ("Starting Python tests") -ForegroundColor "Green"
 .$OPENCOVER -register:user -searchdirs:"$RUNTIME_DIR" -output:py.coverage `
@@ -33,6 +47,9 @@ Write-Host ("Starting Python tests") -ForegroundColor "Green"
 $PYTHON_STATUS = $LastExitCode
 if ($PYTHON_STATUS -ne 0) {
     Write-Host "Python tests failed, continuing to embedded tests" -ForegroundColor "Red"
+    ReportTime ""
+} else {
+    ReportTime "Python tests completed"
 }
 
 # Run Embedded tests with C# coverage
@@ -44,7 +61,10 @@ Write-Host ("Starting embedded tests") -ForegroundColor "Green"
 $CS_STATUS = $LastExitCode
 if ($CS_STATUS -ne 0) {
     Write-Host "Embedded tests failed" -ForegroundColor "Red"
+    ReportTime ""
 } else {
+    ReportTime "Embedded tests completed"
+
     # NuGet for pythonnet-2.3 only has 64-bit binary for Python 3.5
     # the test is only built using modern stack
     if (($env:PLATFORM -eq "x64") -and ($XPLAT) -and ($env:PYTHON_VERSION -eq "3.5")) {
@@ -60,6 +80,9 @@ if ($CS_STATUS -ne 0) {
         $CS_PERF_STATUS = $LastExitCode
         if ($CS_PERF_STATUS -ne 0) {
             Write-Host "Performance tests (C#) failed" -ForegroundColor "Red"
+            ReportTime ""
+        } else {
+            ReportTime "Performance tests (C#) completed"
         }
     } else {
         Write-Host ("Skipping performance tests for ", $env:PYTHON_VERSION) -ForegroundColor "Yellow"
@@ -82,8 +105,13 @@ if ($XPLAT){
     $CS_STATUS = $LastExitCode
     if ($CS_STATUS -ne 0) {
         Write-Host "Embedded tests for netcoreapp2.0 failed" -ForegroundColor "Red"
+        ReportTime ""
+    } else {
+        ReportTime ".NET Core 2.0 tests completed"
     }
 }
+
+Write-Host ($timings | Format-Table | Out-String)
 
 # Set exit code to fail if either Python or Embedded tests failed
 if ($PYTHON_STATUS -ne 0 -or $CS_STATUS -ne 0 -or $CS_PERF_STATUS -ne 0) {
