1ecdce889f6c59176bff06214993db4e161a395c
Félix Bourbonnais <felix.bourbonnais@imaginary-spaces.com>
2020-08-21 12:52:54 -0400

    Incref a valid pointer
    Addresses comment:
    https://github.com/pythonnet/pythonnet/pull/958/#discussion_r390689745

--- src/runtime/methodbinding.cs
+++ src/runtime/methodbinding.cs
@@ -22,11 +22,11 @@ namespace Python.Runtime
             Runtime.XIncref(target);
             this.target = target;
 
-            Runtime.XIncref(targetType);
             if (targetType == IntPtr.Zero)
             {
                 targetType = Runtime.PyObject_Type(target);
             }
+            Runtime.XIncref(targetType);
             this.targetType = targetType;
 
             this.info = null;
