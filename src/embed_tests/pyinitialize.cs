8c133e3c19dd4700b6a876ffc4944412cb3a12db
Félix Bourbonnais <felix.bourbonnais@imaginary-spaces.com>
2020-09-02 14:13:42 -0400

    Fixes for the merge

--- src/embed_tests/pyinitialize.cs
+++ src/embed_tests/pyinitialize.cs
@@ -25,7 +25,7 @@ namespace Python.EmbeddingTest
         {
             using (new PythonEngine())
             {
-                using(var argv = new PyList(new BorrowedReference(Runtime.Runtime.PySys_GetObject("argv"))))
+                using(var argv = new PyList(Runtime.Runtime.PySys_GetObject("argv")))
                 {
                     Assert.AreNotEqual(0, argv.Length());
                 }
@@ -38,7 +38,7 @@ namespace Python.EmbeddingTest
             var args = new[] { "test1", "test2" };
             using (new PythonEngine(args))
             {
-                using(var argv = new PyList(new BorrowedReference(Runtime.Runtime.PySys_GetObject("argv"))))
+                using (var argv = new PyList(Runtime.Runtime.PySys_GetObject("argv")))
                 {
                     Assert.AreEqual(args[0], argv[0].ToString());
                     Assert.AreEqual(args[1], argv[1].ToString());
