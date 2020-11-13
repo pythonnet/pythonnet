094bf2cee96e1996241db92a41b6448e4000bd01
Benedikt Reinartz <filmor@gmail.com>
2020-11-13 08:06:16 +0100

    Fix geninterop script and regenerate interop39.cs
    - Only record structs when they are defined, not when they are declared
    - If a struct was only declared when a typedef was created, it won't
      contain its member declarations. Those have to be drawn from the
      recorded structs instead.
    - Rename internal members of AstParser to make it easier to debug

--- src/runtime/interop39.cs
+++ src/runtime/interop39.cs
@@ -18,6 +18,57 @@ namespace Python.Runtime
     internal static partial class TypeOffset
     {
         // Auto-generated from PyHeapTypeObject in Python.h
+        public static int ob_refcnt = 0;
+        public static int ob_type = 0;
+        public static int ob_size = 0;
+        public static int tp_name = 0;
+        public static int tp_basicsize = 0;
+        public static int tp_itemsize = 0;
+        public static int tp_dealloc = 0;
+        public static int tp_vectorcall_offset = 0;
+        public static int tp_getattr = 0;
+        public static int tp_setattr = 0;
+        public static int tp_as_async = 0;
+        public static int tp_repr = 0;
+        public static int tp_as_number = 0;
+        public static int tp_as_sequence = 0;
+        public static int tp_as_mapping = 0;
+        public static int tp_hash = 0;
+        public static int tp_call = 0;
+        public static int tp_str = 0;
+        public static int tp_getattro = 0;
+        public static int tp_setattro = 0;
+        public static int tp_as_buffer = 0;
+        public static int tp_flags = 0;
+        public static int tp_doc = 0;
+        public static int tp_traverse = 0;
+        public static int tp_clear = 0;
+        public static int tp_richcompare = 0;
+        public static int tp_weaklistoffset = 0;
+        public static int tp_iter = 0;
+        public static int tp_iternext = 0;
+        public static int tp_methods = 0;
+        public static int tp_members = 0;
+        public static int tp_getset = 0;
+        public static int tp_base = 0;
+        public static int tp_dict = 0;
+        public static int tp_descr_get = 0;
+        public static int tp_descr_set = 0;
+        public static int tp_dictoffset = 0;
+        public static int tp_init = 0;
+        public static int tp_alloc = 0;
+        public static int tp_new = 0;
+        public static int tp_free = 0;
+        public static int tp_is_gc = 0;
+        public static int tp_bases = 0;
+        public static int tp_mro = 0;
+        public static int tp_cache = 0;
+        public static int tp_subclasses = 0;
+        public static int tp_weaklist = 0;
+        public static int tp_del = 0;
+        public static int tp_version_tag = 0;
+        public static int tp_finalize = 0;
+        public static int tp_vectorcall = 0;
         public static int am_await = 0;
         public static int am_aiter = 0;
         public static int am_anext = 0;
@@ -174,4 +225,3 @@ namespace Python.Runtime
 
 }
 #endif
-
