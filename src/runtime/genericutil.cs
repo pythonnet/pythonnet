8e754b664a0461fe65a0563cf233ce2de59aeebc
Tom Minka <8955276+tminka@users.noreply.github.com>
2020-12-17 22:10:54 +0000

    Inlined GenericUtil.GenericsByName into GenericByName. Removed unused GenericUtil.GenericsForType. Other code quality improvements.

--- src/runtime/genericutil.cs
+++ src/runtime/genericutil.cs
@@ -9,13 +9,12 @@ namespace Python.Runtime
     /// This class is responsible for efficiently maintaining the bits
     /// of information we need to support aliases with 'nice names'.
     /// </summary>
-    internal class GenericUtil
+    internal static class GenericUtil
     {
+        /// <summary>
+        /// Maps namespace -> generic base name -> list of generic type names
+        /// </summary>
         private static Dictionary<string, Dictionary<string, List<string>>> mapping;
-
-        private GenericUtil()
-        {
-        }
 
         public static void Reset()
         {
@@ -25,6 +24,7 @@ namespace Python.Runtime
         /// <summary>
         /// Register a generic type that appears in a given namespace.
         /// </summary>
+        /// <param name="t">A generic type definition (<c>t.IsGenericTypeDefinition</c> must be true)</param>
         internal static void Register(Type t)
         {
             if (null == t.Namespace || null == t.Name)
@@ -32,22 +32,15 @@ namespace Python.Runtime
                 return;
             }
 
-            Dictionary<string, List<string>> nsmap = null;
-            mapping.TryGetValue(t.Namespace, out nsmap);
-            if (nsmap == null)
+            Dictionary<string, List<string>> nsmap;
+            if (!mapping.TryGetValue(t.Namespace, out nsmap))
             {
                 nsmap = new Dictionary<string, List<string>>();
                 mapping[t.Namespace] = nsmap;
             }
-            string basename = t.Name;
-            int tick = basename.IndexOf("`");
-            if (tick > -1)
-            {
-                basename = basename.Substring(0, tick);
-            }
-            List<string> gnames = null;
-            nsmap.TryGetValue(basename, out gnames);
-            if (gnames == null)
+            string basename = GetBasename(t.Name);
+            List<string> gnames;
+            if (!nsmap.TryGetValue(basename, out gnames))
             {
                 gnames = new List<string>();
                 nsmap[basename] = gnames;
@@ -60,9 +53,8 @@ namespace Python.Runtime
         /// </summary>
         public static List<string> GetGenericBaseNames(string ns)
         {
-            Dictionary<string, List<string>> nsmap = null;
-            mapping.TryGetValue(ns, out nsmap);
-            if (nsmap == null)
+            Dictionary<string, List<string>> nsmap;
+            if (!mapping.TryGetValue(ns, out nsmap))
             {
                 return null;
             }
@@ -85,61 +77,31 @@ namespace Python.Runtime
         /// <summary>
         /// Finds a generic type in the given namespace with the given name and number of generic parameters.
         /// </summary>
-        public static Type GenericByName(string ns, string name, int paramCount)
+        public static Type GenericByName(string ns, string basename, int paramCount)
         {
-            var types = GenericsByName(ns, name);
-            if (types != null)
-            {
-                foreach (Type t in types)
-                {
-                    if (t.GetGenericArguments().Length == paramCount)
-                    {
-                        return t;
-                    }
-                }
-            }
-            return null;
-        }
-
-        public static List<Type> GenericsForType(Type t)
-        {
-            return GenericsByName(t.Namespace, t.Name);
-        }
-
-        public static List<Type> GenericsByName(string ns, string basename)
-        {
-            Dictionary<string, List<string>> nsmap = null;
-            mapping.TryGetValue(ns, out nsmap);
-            if (nsmap == null)
+            Dictionary<string, List<string>> nsmap;
+            if (!mapping.TryGetValue(ns, out nsmap))
             {
                 return null;
             }
 
-            int tick = basename.IndexOf("`");
-            if (tick > -1)
-            {
-                basename = basename.Substring(0, tick);
-            }
-
-            List<string> names = null;
-            nsmap.TryGetValue(basename, out names);
-            if (names == null)
+            List<string> names;
+            if (!nsmap.TryGetValue(GetBasename(basename), out names))
             {
                 return null;
             }
 
-            var result = new List<Type>();
             foreach (string name in names)
             {
                 string qname = ns + "." + name;
                 Type o = AssemblyManager.LookupTypes(qname).FirstOrDefault();
-                if (o != null)
+                if (o != null && o.GetGenericArguments().Length == paramCount)
                 {
-                    result.Add(o);
+                    return o;
                 }
             }
 
-            return result;
+            return null;
         }
 
         /// <summary>
@@ -147,19 +109,31 @@ namespace Python.Runtime
         /// </summary>
         public static string GenericNameForBaseName(string ns, string name)
         {
-            Dictionary<string, List<string>> nsmap = null;
-            mapping.TryGetValue(ns, out nsmap);
-            if (nsmap == null)
+            Dictionary<string, List<string>> nsmap;
+            if (!mapping.TryGetValue(ns, out nsmap))
             {
                 return null;
             }
-            List<string> gnames = null;
+            List<string> gnames;
             nsmap.TryGetValue(name, out gnames);
             if (gnames?.Count > 0)
             {
                 return gnames[0];
             }
             return null;
+        }
+
+        private static string GetBasename(string name)
+        {
+            int tick = name.IndexOf("`");
+            if (tick > -1)
+            {
+                return name.Substring(0, tick);
+            }
+            else
+            {
+                return name;
+            }
         }
     }
 }
