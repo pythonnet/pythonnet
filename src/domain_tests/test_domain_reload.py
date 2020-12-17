fbc06ef2d68a559a1b44e1bb9d85d5887bf37723
Félix Bourbonnais <felix.bourbonnais@imaginary-spaces.com>
2020-12-17 15:48:22 -0500

    fixup! Skip the domain reload tests on macos

--- src/domain_tests/test_domain_reload.py
+++ src/domain_tests/test_domain_reload.py
@@ -17,72 +17,72 @@ def _run_test(testname):
 
     assert proc.returncode == 0
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_class():
     _run_test('class_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_class_member_static_function():
     _run_test('static_member_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_class_member_function():
     _run_test('member_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_class_member_field():
     _run_test('field_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_class_member_property():
     _run_test('property_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_rename_namespace():
     _run_test('namespace_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_field_visibility_change():
     _run_test("field_visibility_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_method_visibility_change():
     _run_test("method_visibility_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_property_visibility_change():
     _run_test("property_visibility_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_class_visibility_change():
     _run_test("class_visibility_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_method_parameters_change():
     _run_test("method_parameters_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_method_return_type_change():
     _run_test("method_return_type_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_field_type_change():
     _run_test("field_type_change")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 @pytest.mark.xfail(reason="Events not yet serializable")
 def test_rename_event():
     _run_test('event_rename')
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 @pytest.mark.xfail(reason="newly instanced object uses PyType_GenericAlloc")
 def test_construct_removed_class():
     _run_test("construct_removed_class")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_out_to_ref_param():
     _run_test("out_to_ref_param")
 
-@pytest.mark.skipif(platform.system == 'Darwin', reason='FIXME: macos can\'t find the python library')
+@pytest.mark.skipif(platform.system() == 'Darwin', reason='FIXME: macos can\'t find the python library')
 def test_nested_type():
     _run_test("nested_type")
