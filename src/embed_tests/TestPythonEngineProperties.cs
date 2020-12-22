1f40564cbff0f109b6042973527b032be194b7e8
Tom Minka <8955276+tminka@users.noreply.github.com>
2020-12-22 17:05:40 +0000

    TestPythonEngineProperties.SetPythonPath avoids corrupting the module search path for later tests (#1336)
    SetPythonPath uses System.IO.Path.PathSeparator and imports a module under the new path

--- src/embed_tests/TestPythonEngineProperties.cs
+++ src/embed_tests/TestPythonEngineProperties.cs
@@ -181,13 +181,37 @@ namespace Python.EmbeddingTest
         public void SetPythonPath()
         {
             PythonEngine.Initialize();
-            string path = PythonEngine.PythonPath;
+
+            const string moduleName = "pytest";
+            bool importShouldSucceed;
+            try
+            {
+                Py.Import(moduleName);
+                importShouldSucceed = true;
+            }
+            catch
+            {
+                importShouldSucceed = false;
+            }
+
+            string[] paths = Py.Import("sys").GetAttr("path").As<string[]>();
+            string path = string.Join(System.IO.Path.PathSeparator.ToString(), paths);
+
+            // path should not be set to PythonEngine.PythonPath here.
+            // PythonEngine.PythonPath gets the default module search path, not the full search path.
+            // The list sys.path is initialized with this value on interpreter startup;
+            // it can be (and usually is) modified later to change the search path for loading modules.
+            // See https://docs.python.org/3/c-api/init.html#c.Py_GetPath
+            // After PythonPath is set, then PythonEngine.PythonPath will correctly return the full search path. 
+
             PythonEngine.Shutdown();
 
             PythonEngine.PythonPath = path;
             PythonEngine.Initialize();
 
             Assert.AreEqual(path, PythonEngine.PythonPath);
+            if (importShouldSucceed) Py.Import(moduleName);
+
             PythonEngine.Shutdown();
         }
     }
