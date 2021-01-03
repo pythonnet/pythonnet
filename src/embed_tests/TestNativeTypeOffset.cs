48c63e779734a2b84fdd441dfba4e9b4c6dddbbe
Tom Minka <8955276+tminka@users.noreply.github.com>
2021-01-03 16:10:27 +0000

    LoadNativeTypeOffsetClass checks for sys.abiflags

--- src/embed_tests/TestNativeTypeOffset.cs
+++ src/embed_tests/TestNativeTypeOffset.cs
@@ -7,21 +7,39 @@ using System.Threading.Tasks;
 
 using NUnit.Framework;
 
+using Python.Runtime;
+
 namespace Python.EmbeddingPythonTest
 {
     public class TestNativeTypeOffset
     {
-#if WINDOWS
-        // The code for NativeTypeOffset is not generated under Windows because sys.abiflags does not exist (see setup.py)
-#else
+        private Py.GILState _gs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gs = Py.GIL();
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            _gs.Dispose();
+        }
+
         /// <summary>
         /// Tests that installation has generated code for NativeTypeOffset and that it can be loaded.
         /// </summary>        
         [Test]
         public void LoadNativeTypeOffsetClass()
         {
-            new Python.Runtime.NativeTypeOffset();
+            PyObject sys = Py.Import("sys");
+            string attributeName = "abiflags";
+            if (sys.HasAttr(attributeName) && !string.IsNullOrEmpty(sys.GetAttr(attributeName).ToString()))
+            {
+                string typeName = "Python.Runtime.NativeTypeOffset";
+                Assert.NotNull(Type.GetType(typeName), $"{typeName} does not exist and sys.{attributeName} is not empty");
+            }
         }
-#endif
     }
 }
