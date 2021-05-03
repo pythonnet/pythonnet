33d771c2804d82a9c4cf740a4564338d32e37e1d
Félix Bourbonnais <felix.bourbonnais@imaginary-spaces.com>
2021-05-03 03:08:43 -0400

    Re-enable the domain reload tests (#1404)
    Add `domain_tests` to `testpaths` in pyproject.toml
    Run domain tests only if domains are supported
    Move the domain reload tests to the `tests` directory, merge the `conftest.py` files
    
    * Don't run domain reload tests in soft mode, increase timeout

--- src/domain_tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from subprocess import check_call
-# test_proj_path = os.path.join(cwd, "..", "testing")
-cfd = os.path.dirname(__file__)
-bin_path = os.path.join(cfd, 'bin')
-check_call(["dotnet", "build", cfd, '-o', bin_path])
