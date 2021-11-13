9c62d647ffa0a1dd68bee25c378e07607d0cae49
Benedikt Reinartz <filmor@gmail.com>
2021-11-13 15:49:14 +0100

    Include NonCopyableAnalyzer

---
+++ src/noncopyable_analyzer/NonCopyableAnalyzer.cs
@@ -0,0 +1,252 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace NonCopyable
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NonCopyableAnalyzer : DiagnosticAnalyzer
+    {
+        private static DiagnosticDescriptor CreateRule(int num, string type)
+            => new DiagnosticDescriptor("NoCopy" + num.ToString("00"), "non-copyable", "🚫 " + type + ". '{0}' is non-copyable.", "Correction", DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        private static DiagnosticDescriptor FieldDeclarationRule = CreateRule(1, "field declaration");
+        private static DiagnosticDescriptor InitializerRule = CreateRule(2, "initializer");
+        private static DiagnosticDescriptor AssignmentRule = CreateRule(3, "assignment");
+        private static DiagnosticDescriptor ArgumentRule = CreateRule(4, "argument");
+        private static DiagnosticDescriptor ReturnRule = CreateRule(5, "return");
+        private static DiagnosticDescriptor ConversionRule = CreateRule(6, "conversion");
+        private static DiagnosticDescriptor PatternRule = CreateRule(7, "pattern matching");
+        private static DiagnosticDescriptor TupleRule = CreateRule(8, "tuple");
+        private static DiagnosticDescriptor MemberRule = CreateRule(9, "member reference");
+        private static DiagnosticDescriptor ReadOnlyInvokeRule = CreateRule(10, "readonly invoke");
+        private static DiagnosticDescriptor GenericConstraintRule = CreateRule(11, "generic constraint");
+        private static DiagnosticDescriptor DelegateRule = CreateRule(12, "delegate");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(FieldDeclarationRule, InitializerRule, AssignmentRule, ArgumentRule, ReturnRule, ConversionRule, PatternRule, TupleRule, MemberRule, ReadOnlyInvokeRule, GenericConstraintRule, DelegateRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(csc =>
+            {
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (ISymbolInitializerOperation)oc.Operation;
+                    CheckCopyability(oc, op.Value, InitializerRule);
+                }, OperationKind.FieldInitializer,
+                OperationKind.ParameterInitializer,
+                OperationKind.PropertyInitializer,
+                OperationKind.VariableInitializer);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    // including member initializer
+                    // including collection element initializer
+                    var op = (ISimpleAssignmentOperation)oc.Operation;
+                    if (op.IsRef) return;
+                    CheckCopyability(oc, op.Value, AssignmentRule);
+                }, OperationKind.SimpleAssignment);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    // including non-ref extension method invocation
+                    var op = (IArgumentOperation)oc.Operation;
+                    if (op.Parameter.RefKind != RefKind.None) return;
+                    CheckCopyability(oc, op.Value, ArgumentRule);
+                }, OperationKind.Argument);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (IReturnOperation)oc.Operation;
+                    if (op.ReturnedValue == null) return;
+                    CheckCopyability(oc, op.ReturnedValue, ReturnRule);
+                }, OperationKind.Return,
+                OperationKind.YieldReturn);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (IConversionOperation)oc.Operation;
+                    var v = op.Operand;
+                    if (v.Kind == OperationKind.DefaultValue) return;
+                    var t = v.Type;
+                    if (!t.IsNonCopyable()) return;
+
+                    if (op.OperatorMethod != null && op.OperatorMethod.Parameters.Length == 1)
+                    {
+                        var parameter = op.OperatorMethod.Parameters[0];
+                        if (parameter.RefKind != RefKind.None) return;
+                    }
+
+                    if (op.Parent is IForEachLoopOperation &&
+                        op == ((IForEachLoopOperation)op.Parent).Collection &&
+                        op.Conversion.IsIdentity)
+                    {
+                        return;
+                    }
+
+                    oc.ReportDiagnostic(Diagnostic.Create(ConversionRule, v.Syntax.GetLocation(), t.Name));
+                }, OperationKind.Conversion);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (IArrayInitializerOperation)oc.Operation;
+
+                    if (!((IArrayTypeSymbol)((IArrayInitializerOperation)op.Parent).Type).ElementType.IsNonCopyable()) return;
+
+                    foreach (var v in op.ElementValues)
+                    {
+                        CheckCopyability(oc, v, InitializerRule);
+                    }
+                }, OperationKind.ArrayInitializer);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (ICollectionElementInitializerOperation)oc.Operation;
+
+                    if (!HasNonCopyableParameter(op.AddMethod)) return;
+
+                    foreach (var a in op.Arguments)
+                    {
+                        CheckCopyability(oc, a, InitializerRule);
+                    }
+                }, OperationKind.CollectionElementInitializer);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (IDeclarationPatternOperation)oc.Operation;
+                    var t = ((ILocalSymbol)op.DeclaredSymbol).Type;
+                    if (!t.IsNonCopyable()) return;
+                    oc.ReportDiagnostic(Diagnostic.Create(PatternRule, op.Syntax.GetLocation(), t.Name));
+                }, OperationKind.DeclarationPattern);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    var op = (ITupleOperation)oc.Operation;
+
+                    // exclude ParenthesizedVariableDesignationSyntax
+                    if (op.Syntax.Kind() != SyntaxKind.TupleExpression) return;
+
+                    foreach (var v in op.Elements)
+                    {
+                        CheckCopyability(oc, v, TupleRule);
+                    }
+                }, OperationKind.Tuple);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    // instance property/event should not be referenced with in parameter/ref readonly local/readonly field
+                    var op = (IMemberReferenceOperation)oc.Operation;
+                    CheckInstanceReadonly(oc, op.Instance, MemberRule);
+                }, OperationKind.PropertyReference,
+                OperationKind.EventReference);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    // instance method should not be invoked with in parameter/ref readonly local/readonly field
+                    var op = (IInvocationOperation)oc.Operation;
+
+                    CheckGenericConstraints(oc, op, GenericConstraintRule);
+                    CheckInstanceReadonly(oc, op.Instance, ReadOnlyInvokeRule);
+
+                }, OperationKind.Invocation);
+
+                csc.RegisterOperationAction(oc =>
+                {
+                    // delagate creation
+                    var op = (IMemberReferenceOperation)oc.Operation;
+                    if (op.Instance == null) return;
+                    if (!op.Instance.Type.IsNonCopyable()) return;
+                    oc.ReportDiagnostic(Diagnostic.Create(DelegateRule, op.Instance.Syntax.GetLocation(), op.Instance.Type.Name));
+                }, OperationKind.MethodReference);
+
+                csc.RegisterSymbolAction(sac =>
+                {
+                    var f = (IFieldSymbol)sac.Symbol;
+                    if (f.IsStatic) return;
+                    if (!f.Type.IsNonCopyable()) return;
+                    if (f.ContainingType.IsReferenceType) return;
+                    if (f.ContainingType.IsNonCopyable()) return;
+                    sac.ReportDiagnostic(Diagnostic.Create(FieldDeclarationRule, f.DeclaringSyntaxReferences[0].GetSyntax().GetLocation(), f.Type.Name));
+                }, SymbolKind.Field);
+            });
+
+            // not supported yet:
+            //    OperationKind.CompoundAssignment,
+            //    OperationKind.UnaryOperator,
+            //    OperationKind.BinaryOperator,
+        }
+
+        private static void CheckGenericConstraints(in OperationAnalysisContext oc, IInvocationOperation op, DiagnosticDescriptor rule)
+        {
+            var m = op.TargetMethod;
+
+            if (m.IsGenericMethod)
+            {
+                var parameters = m.TypeParameters;
+                var arguments = m.TypeArguments;
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    var p = parameters[i];
+                    var a = arguments[i];
+
+                    if (a.IsNonCopyable() && !p.IsNonCopyable())
+                        oc.ReportDiagnostic(Diagnostic.Create(rule, op.Syntax.GetLocation(), a.Name));
+                }
+            }
+        }
+
+        private static void CheckInstanceReadonly(in OperationAnalysisContext oc, IOperation instance, DiagnosticDescriptor rule)
+        {
+            if (instance == null) return;
+
+            var t = instance.Type;
+            if (!t.IsNonCopyable()) return;
+
+            if (IsInstanceReadonly(instance))
+            {
+                oc.ReportDiagnostic(Diagnostic.Create(rule, instance.Syntax.GetLocation(), t.Name));
+            }
+        }
+
+        private static bool IsInstanceReadonly(IOperation instance)
+        {
+            bool isReadOnly = false;
+            switch (instance)
+            {
+                case IFieldReferenceOperation r:
+                    isReadOnly = r.Field.IsReadOnly;
+                    break;
+                case ILocalReferenceOperation r:
+                    isReadOnly = r.Local.RefKind == RefKind.In;
+                    break;
+                case IParameterReferenceOperation r:
+                    isReadOnly = r.Parameter.RefKind == RefKind.In;
+                    break;
+            }
+
+            return isReadOnly;
+        }
+
+        private static bool HasNonCopyableParameter(IMethodSymbol m)
+        {
+            foreach (var p in m.Parameters)
+            {
+                if(p.RefKind == RefKind.None)
+                {
+                    if (p.Type.IsNonCopyable()) return true;
+                }
+            }
+            return false;
+        }
+
+        private static void CheckCopyability(in OperationAnalysisContext oc, IOperation v, DiagnosticDescriptor rule)
+        {
+            var t = v.Type;
+            if (!t.IsNonCopyable()) return;
+            if (v.CanCopy()) return;
+            oc.ReportDiagnostic(Diagnostic.Create(rule, v.Syntax.GetLocation(), t.Name));
+        }
+    }
+}
---
+++ src/noncopyable_analyzer/TypeExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace NonCopyable
+{
+    public static class TypeExtensions
+    {
+        public static bool IsNonCopyable(this ITypeSymbol t)
+        {
+            if (t == null) return false;
+            if (t.TypeKind != TypeKind.Struct && t.TypeKind != TypeKind.TypeParameter) return false;
+
+            if (HasNonCopyableAttribute(t)) return true;
+
+            if (t.TypeKind == TypeKind.Struct)
+            {
+                foreach (var ifType in t.AllInterfaces)
+                {
+                    if (HasNonCopyableAttribute(ifType)) return true;
+                }
+            }
+            else
+            {
+                foreach (var constraint in ((ITypeParameterSymbol)t).ConstraintTypes)
+                {
+                    if (HasNonCopyableAttribute(constraint)) return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        private static bool HasNonCopyableAttribute(ITypeSymbol t)
+        {
+            foreach (var a in t.GetAttributes())
+            {
+                var str = a.AttributeClass.Name;
+                if (str.EndsWith("NonCopyable") || str.EndsWith("NonCopyableAttribute")) return true;
+            }
+
+            return false;
+        }
+
+        private static SyntaxList<AttributeListSyntax> GetAttributes(this SyntaxNode syntax)
+        {
+            switch (syntax)
+            {
+                case StructDeclarationSyntax s:
+                    return s.AttributeLists;
+                case TypeParameterSyntax s:
+                    return s.AttributeLists;
+                default:
+                    return default;
+            }
+        }
+
+        /// <summary>
+        /// test whether op is copyable or not even when it is a non-copyable instance.
+        /// </summary>
+        public static bool CanCopy(this IOperation op)
+        {
+            var k = op.Kind;
+
+            if (k == OperationKind.Conversion)
+            {
+                var operandKind = ((IConversionOperation)op).Operand.Kind;
+                // default literal (invalid if LangVersion < 7.1)
+                if (operandKind == OperationKind.DefaultValue || operandKind == OperationKind.Invalid) return true;
+            }
+
+            if (k == OperationKind.LocalReference || k == OperationKind.FieldReference || k == OperationKind.PropertyReference || k == OperationKind.ArrayElementReference)
+            {
+                //need help: how to get ref-ness from IOperation?
+                var parent = op.Syntax.Parent.Kind();
+                if (parent == SyntaxKind.RefExpression) return true;
+            }
+
+            return k == OperationKind.ObjectCreation
+                || k == OperationKind.DefaultValue
+                || k == OperationKind.Literal
+                || k == OperationKind.Invocation;
+
+            //todo: should return value be OK?
+            //todo: move semantics
+        }
+    }
+}
