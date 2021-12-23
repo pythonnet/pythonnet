f0dbb50eca72dce16bd85b5dccaa2c0c9243bd21
Benedikt Reinartz <filmor@gmail.com>
2021-12-23 23:42:04 +0100

    Fix the PyGILState_STATE type
    CPython uses a bare `enum` here, both of .NET and C default to `int`
    enums, so this should be closer to the truth if no special compile
    options are used.

--- src/runtime/native/PyGILState.cs
+++ src/runtime/native/PyGILState.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Runtime.InteropServices;
-
 namespace Python.Runtime.Native;
 
 /// <remarks><c>PyGILState_STATE</c></remarks>
-[StructLayout(LayoutKind.Sequential)]
-struct PyGILState
+enum PyGILState
 {
-    IntPtr handle;
+    PyGILState_LOCKED,
+    PyGILState_UNLOCKED 
 }
