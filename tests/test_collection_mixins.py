cf8823fa941842cfd19ad808f04bc6aa6d0b7970
Victor Nova <lost@losttech.software>
2022-05-07 10:03:50 -0700

    added regression test for "in Dictionary.Keys" check

--- tests/test_collection_mixins.py
+++ tests/test_collection_mixins.py
@@ -14,3 +14,10 @@ def test_dict_items():
     k,v = items[0]
     assert k == 42
     assert v == "a"
+
+# regression test for https://github.com/pythonnet/pythonnet/issues/1785
+def test_dict_in_keys():
+    d = C.Dictionary[str, int]()
+    d["a"] = 42
+    assert "a" in d.Keys
+    assert "b" not in d.Keys
