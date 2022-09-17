91d3e55f965ae7fcaf9845d48c3076149dd5a712
Victor <lost@losttech.software>
2022-09-17 01:48:28 -0700

    reenabled test for https://github.com/pythonnet/pythonnet/issues/1256 : NoStackOverflowOnSystemType (#1943)

--- src/embed_tests/Codecs.cs
+++ src/embed_tests/Codecs.cs
@@ -285,7 +285,6 @@ namespace Python.EmbeddingTest {
         }
 
         // regression for https://github.com/pythonnet/pythonnet/issues/1427
-        [Ignore("https://github.com/pythonnet/pythonnet/issues/1256")]
         [Test]
         public void PythonRegisteredDecoder_NoStackOverflowOnSystemType()
         {
