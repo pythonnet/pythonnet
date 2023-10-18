daecf43009faea9fabd0ef50aad0377943050a87
Benedikt Reinartz <filmor@gmail.com>
2023-10-18 15:14:04 +0200

    Explicitly compare MaybeType objects by name

--- src/runtime/StateSerialization/MaybeType.cs
+++ src/runtime/StateSerialization/MaybeType.cs
@@ -3,62 +3,71 @@ using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Collections.Generic;
 
-namespace Python.Runtime
+namespace Python.Runtime;
+
+[Serializable]
+internal struct MaybeType : ISerializable
 {
-    [Serializable]
-    internal struct MaybeType : ISerializable
+    public static implicit operator MaybeType (Type ob) => new(ob);
+
+    // The AssemblyQualifiedName of the serialized Type
+    const string SerializationName = "n";
+    readonly string name;
+    readonly Type type;
+
+    public string DeletedMessage
     {
-        public static implicit operator MaybeType (Type ob) => new(ob);
-
-        // The AssemblyQualifiedName of the serialized Type
-        const string SerializationName = "n";
-        readonly string name;
-        readonly Type type;
-
-        public string DeletedMessage
+        get
         {
-            get
-            {
-                return $"The .NET Type {name} no longer exists";
-            }
-        }
-
-        public Type Value
-        {
-            get
-            {
-                if (type == null)
-                {
-                    throw new SerializationException(DeletedMessage);
-                }
-                return type;
-            }
-        }
-
-        public string Name => name;
-        public bool Valid => type != null;
-
-        public override string ToString()
-        {
-            return (type != null ? type.ToString() : $"missing type: {name}");
-        }
-
-        public MaybeType(Type tp)
-        {
-            type = tp;
-            name = tp.AssemblyQualifiedName;
-        }
-
-        private MaybeType(SerializationInfo serializationInfo, StreamingContext context)
-        {
-            name = (string)serializationInfo.GetValue(SerializationName, typeof(string));
-            type = Type.GetType(name, throwOnError:false);
-        }
-
-        public void GetObjectData(SerializationInfo serializationInfo, StreamingContext context)
-        {
-            serializationInfo.AddValue(SerializationName, name);
+            return $"The .NET Type {name} no longer exists";
         }
     }
+
+    public Type Value
+    {
+        get
+        {
+            if (type == null)
+            {
+                throw new SerializationException(DeletedMessage);
+            }
+            return type;
+        }
+    }
+
+    public string Name => name;
+    public bool Valid => type != null;
+
+    public override string ToString()
+    {
+        return (type != null ? type.ToString() : $"missing type: {name}");
+    }
+
+    public MaybeType(Type tp)
+    {
+        type = tp;
+        name = tp.AssemblyQualifiedName;
+    }
+
+    private MaybeType(SerializationInfo serializationInfo, StreamingContext context)
+    {
+        name = (string)serializationInfo.GetValue(SerializationName, typeof(string));
+        type = Type.GetType(name, throwOnError:false);
+    }
+
+    public void GetObjectData(SerializationInfo serializationInfo, StreamingContext context)
+    {
+        serializationInfo.AddValue(SerializationName, name);
+    }
+}
+
+[Serializable]
+internal class MaybeTypeComparer : IEqualityComparer<MaybeType>
+{
+    public bool Equals (MaybeType lhs, MaybeType rhs) =>
+        lhs.Name == rhs.Name;
+
+    public int GetHashCode(MaybeType t) => (t.Name ?? "").GetHashCode();
 }
